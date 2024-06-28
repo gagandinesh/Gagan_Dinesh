5b7025f8d1426b2cd53c554ed27b7a0e1fbd96d9
gagandinesh <133633833+gagandinesh@users.noreply.github.com>
2024-06-28 17:06:17 +0530

    Portfolio

---
+++ Gagan_Dinesh/Main.js
@@ -0,0 +1,9 @@
+var Typed = new Typed(".text ", {
+    strings:["Software Engineer", "Python Developer", "ML Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
